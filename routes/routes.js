babd2a1234f9b1fa7b20d2137ed86b1a788df81b
joseph mccarthy <jem2018@umd.edu>
2021-04-06 22:06:32 -0400

    oeueoueo

---
+++ routes/routes.js
@@ -0,0 +1,369 @@
+/* eslint-disable no-console */
+import express from 'express';
+import sequelize from 'sequelize';
+
+import db from '../database/initializeDB.js';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('group 8 api');
+});
+
+/// /////////////////////////////////
+/// ////Diseases Endpoints////////
+/// /////////////////////////////////
+router.get('/diseases', async (req, res) => {
+  try {
+    const disease = await db.diseases.findAll();
+    const reply = disease.length > 0 ? { data: disease } : { message: 'no results found' };
+    res.json(reply);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.get('/diseases/:disease_id', async (req, res) => {
+  try {
+    const disease = await db.diseases.findAll({
+      where: {
+        disease_id: req.params.disease_id
+      }
+    });
+
+    res.json(disease);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.post('/diseases', async (req, res) => {
+  const disease = await db.diseases.findAll();
+  const currentId = (await disease.length) + 1;
+  try {
+    const newDisease = await db.diseases.create({
+      disease_id: currentId,
+      disease_name: req.body.disease_name,
+      latin_name: req.body.latin_name,
+      type_of_infection: req.body.type_of_infection,
+      disease_realm: req.body.disease_realm,
+      disease_phylum: req.body.disease_phylum,
+      disease_class: req.body.disease_class,
+      disease_order: req.body.disease_order,
+      disease_family: req.body.disease_family,
+      disease_genus: req.body.disease_genus,
+      disease_species: req.body.disease_species      
+    });
+    res.json(newDisease);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.delete('/diseases/:disease_id', async (req, res) => {
+  try {
+    await db.disease.destroy({
+      where: {
+        disease_id: req.params.disease_id
+      }
+    });
+    res.send('Successfully Deleted');
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.put('/diseases', async (req, res) => {
+  try {
+    await db.disease.update(
+      {
+        disease_name: req.body.disease_name,
+        latin_name: req.body.latin_name,
+        type_of_infection: req.body.type_of_infection,
+        disease_realm: req.body.disease_realm,
+        disease_phylum: req.body.disease_phylum,
+        disease_class: req.body.disease_class,
+        disease_order: req.body.disease_order,
+        disease_family: req.body.disease_family,
+        disease_genus: req.body.disease_genus,
+        disease_species: req.body.disease_species 
+      },
+      {
+        where: {
+          disease_id: req.body.disease_id
+        }
+      }
+    );
+    res.send('Successfully Updated');
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+/// /////////////////////////////////
+/// ////////Countries to outbreaks Endpoints//////////
+/// /////////////////////////////////
+router.get('/countries_to_outbreaks', async (req, res) => {
+  try {
+    const meals = await db.countries_to_outbreaks.findAll();
+    res.json(meals);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.get('/countries_to_outbreaks/:country_id', async (req, res) => {
+  try {
+    const meals = await db.countries_to_outbreaks.findAll({
+      where: {
+        country_id: req.params.country_id
+      }
+    });
+    res.json(meals);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.put('/countries_to_outbreaks', async (req, res) => {
+  try {
+    await db.countries_to_outbreaks.update(
+      {
+        meal_name: req.body.meal_name,
+        meal_category: req.body.meal_category
+      },
+      {
+        where: {
+          meal_id: req.body.meal_id
+        }
+      }
+    );
+    res.send('Meal Successfully Updated');
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+/// /////////////////////////////////
+/// ////////Countries Endpoints/////////
+/// /////////////////////////////////
+router.get('/countries', async (req, res) => {
+  try {
+    const macros = await db.countries.findAll();
+    res.send(macros);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.get('/countries/:country_id', async (req, res) => {
+  try {
+    const meals = await db.countries.findAll({
+      where: {
+        country_id: req.params.country_id
+      }
+    });
+    res.json(meals);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.put('/macros', async (req, res) => {
+  try {
+    // N.B. - this is a good example of where to use code validation to confirm objects
+    await db.Macros.update(
+      {
+        meal_name: req.body.meal_name,
+        meal_category: req.body.meal_category,
+        calories: req.body.calories,
+        serving_size: req.body.serving_size,
+        cholesterol: req.body.cholesterol,
+        sodium: req.body.sodium,
+        carbs: req.body.carbs,
+        protein: req.body.protein,
+        fat: req.body.fat
+      },
+      {
+        where: {
+          meal_id: req.body.meal_id
+        }
+      }
+    );
+    res.send('Successfully Updated');
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+/// /////////////////////////////////
+/// outbreaks Endpoints///
+/// /////////////////////////////////
+router.get('/outbreaks', async (req, res) => {
+  try {
+    const restrictions = await db.outbreaks.findAll();
+    res.json(restrictions);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.get('/outbreaks/:outbreak_id', async (req, res) => {
+  try {
+    const restrictions = await db.outbreaks.findAll({
+      where: {
+        outbreak_id: req.params.outbreak_id
+      }
+    });
+    res.json(restrictions);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+/// /////////////////////////////////
+/// reports Endpoints///
+/// /////////////////////////////////
+router.get('/reports', async (req, res) => {
+  try {
+    const restrictions = await db.reports.findAll();
+    res.json(restrictions);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.get('/reports/:report_id', async (req, res) => {
+  try {
+    const restrictions = await db.reports.findAll({
+      where: {
+        Report_id: req.params.Report_id
+      }
+    });
+    res.json(restrictions);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+/// /////////////////////////////////
+/// treatments_to_diseases Endpoints///
+/// /////////////////////////////////
+router.get('/treatments_to_diseases', async (req, res) => {
+  try {
+    const restrictions = await db.treatments_to_diseases.findAll();
+    res.json(restrictions);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.get('/treatments_to_diseases/:disease_id', async (req, res) => {
+  try {
+    const restrictions = await db.treatments_to_diseases.findAll({
+      where: {
+        disease_id: req.params.disease_id
+      }
+    });
+    res.json(restrictions);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+
+/// /////////////////////////////////
+/// treatments Endpoints///
+/// /////////////////////////////////
+router.get('/treatments', async (req, res) => {
+  try {
+    const restrictions = await db.treatments.findAll();
+    res.json(restrictions);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.get('/treatments/:vaccine_name', async (req, res) => {
+  try {
+    const restrictions = await db.treatments.findAll({
+      where: {
+        vaccine_name: req.params.vaccine_name
+      }
+    });
+    res.json(restrictions);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+/// //////////////////////////////////
+/// ///////Custom SQL Endpoint////////
+/// /////////////////////////////////
+const macrosCustom = 'SELECT `Dining_Hall_Tracker`.`Meals`.`meal_id` AS `meal_id`,`Dining_Hall_Tracker`.`Meals`.`meal_name` AS `meal_name`,`Dining_Hall_Tracker`.`Macros`.`calories` AS `calories`,`Dining_Hall_Tracker`.`Macros`.`carbs` AS `carbs`,`Dining_Hall_Tracker`.`Macros`.`sodium` AS `sodium`,`Dining_Hall_Tracker`.`Macros`.`protein` AS `protein`,`Dining_Hall_Tracker`.`Macros`.`fat` AS `fat`,`Dining_Hall_Tracker`.`Macros`.`cholesterol` AS `cholesterol`FROM(`Dining_Hall_Tracker`.`Meals`JOIN `Dining_Hall_Tracker`.`Macros`)WHERE(`Dining_Hall_Tracker`.`Meals`.`meal_id` = `Dining_Hall_Tracker`.`Macros`.`meal_id`)';
+router.get('/table/data', async (req, res) => {
+  try {
+    const result = await db.sequelizeDB.query(macrosCustom, {
+      type: sequelize.QueryTypes.SELECT
+    });
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+const mealMapCustom = `SELECT hall_name,
+  hall_address,
+  hall_lat,
+  hall_long,
+  meal_name
+FROM
+  Meals m
+INNER JOIN Meals_Locations ml 
+  ON m.meal_id = ml.meal_id
+INNER JOIN Dining_Hall d
+ON d.hall_id = ml.hall_id;`;
+router.get('/map/data', async (req, res) => {
+  try {
+    const result = await db.sequelizeDB.query(mealMapCustom, {
+      type: sequelize.QueryTypes.SELECT
+    });
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+router.get('/custom', async (req, res) => {
+  try {
+    const result = await db.sequelizeDB.query(req.body.query, {
+      type: sequelize.QueryTypes.SELECT
+    });
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+export default router;
