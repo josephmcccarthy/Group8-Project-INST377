6a6f644cb1023167f31ce41ceff7569f35d05f51
joseph mccarthy <jem2018@umd.edu>
2021-04-06 20:33:22 -0400

    aoeh

---
+++ models/countries.js
@@ -0,0 +1,24 @@
+export default (sequelize, DataTypes) => {
+    const countries = sequelize.define(
+      'countries',
+      {
+        country_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true
+        },
+        country_name: {
+          type: DataTypes.STRING
+        },
+        historical_name: {
+            type: DataTypes.STRING
+          },
+        country_code: {
+          type: DataTypes.STRING
+        }
+      },
+      { freezeTableName: true, timestamps: false }
+    );
+    return countries;
+  };
+  
