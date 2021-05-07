c508e3dfb8a0c712597e453710fff2151db361cc
Avaldesuso <78240612+Avaldesuso@users.noreply.github.com>
2021-05-07 00:19:01 -0400

    Update graphs.js

--- public/graphs.js
+++ public/graphs.js
@@ -1,1 +1,24 @@
 //Work on the front end graphs here. 
+window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      title:{
+        text: "diseases count"
+      },
+      data: [
+      {
+        type: "bar",
+        dataPoints: [
+        { y: /*numeric data here*/, label: /*disease name*/},
+        { y: /*numeric data here*/, label: /*disease name*/},
+        { y: /*numeric data here*/, label: /*disease name*/},
+        { y: /*numeric data here*/, label: /*disease name*/},
+        { y: /*numeric data here*/, label: /*disease name*/},
+        { y: /*numeric data here*/, label: /*disease name*/}
+        ]
+      }
+      ]
+    });
+
+chart.render();
+}
